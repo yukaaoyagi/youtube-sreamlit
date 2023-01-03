596a2c89cea97f8e7644e86c665a1e35f4a58845
Yuka Aoyagi <yuka.aoyagi.0428@gmail.com>
2023-01-03 17:45:11 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import time
+
+st.title("streamlit 超入門")
+
+st.write("プログレスバーの表示")
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラムです')
+
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ内容を書く')
+
